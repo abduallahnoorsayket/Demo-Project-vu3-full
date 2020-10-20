6dfc9db35a61de6569136017733af5be6f56952f
abduallahnoorsayket <abdullahnoorsayket@gmail.com>
2020-10-20 17:27:28 +0600

    without styling

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,38 @@
 <template>
-  <div>
-    <h1> DC heros :</h1>
+<div class="text-center">
+    <h1> DC heros Members {{herosCount}} </h1>
     <ul>
     
-      <li v-for="(hero,index) in dcHeros "  :key="hero">{{index}}  {{hero.name}}</li>
+      <li v-for="(hero,index) in dcHeros "  :key="hero.name">
+        <p>{{hero.name}}  <button v-on:click="remove(index)">x</button></p> 
+        
+        </li>
     </ul>
-    <input :[attribute]="newHero">
-    <button :disabled="isActive">Add hero</button>
+  <form @submit.prevent="addHero">
+     
+      <input v-model.trim="newHero" placeholder="Enter your hero ">
+      <button type="submit">Add hero</button> <br>
+      
+  </form>
+  <small> Total hereos in the list {{herosCount}}</small>
   </div>
+ 
 </template>
 
-<script>
+<script> 
 export default {
+  computed:{
+    herosCount(){
+      return this.dcHeros.length;
+    }
+      
+  },
   data(){
    
     return{
-      attribute: "value",
+      
       isActive: false,
-       newHero:"Aqaman mister",
+       newHero:"",
       dcHeros: [
         {name:"superman"},
         {name:"flash"},
@@ -26,6 +41,18 @@ export default {
       ]
               
     }
+  },
+  methods:{
+    addHero(){
+    if(this.newHero != ''){
+            this.dcHeros.push({name:this .newHero});
+          this.newHero='';
+    }
+    },
+    remove(index){
+       this.dcHeros  = this.dcHeros.filter((hero,i) => i != index);
+    }
+
   }
 
 }
